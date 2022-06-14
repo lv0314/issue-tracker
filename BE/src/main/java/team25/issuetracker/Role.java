07ad71a67c903532df8c4d4bd671677d12e88790
tokaji <fpzk5656@gmail.com>
2022-06-15 06:38:11 +0900

    feat: Role enum 추가

---
+++ BE/src/main/java/team25/issuetracker/Role.java
@@ -0,0 +1,16 @@
+package team25.issuetracker;
+
+public enum Role {
+	GUEST("ROLE_GUEST"),
+	USER("ROLE_USER");
+
+	private final String key;
+
+	Role(String key) {
+		this.key = key;
+	}
+
+	public String getKey() {
+		return key;
+	}
+}
