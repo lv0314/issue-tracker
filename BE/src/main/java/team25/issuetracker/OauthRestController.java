8170b50b756d91a7accbc4df7976d91e3677f121
tokaji <fpzk5656@gmail.com>
2022-06-15 06:25:15 +0900

    feat: provider 설정하면 해당 되는 authorization code 받는 컨트롤러 추가

---
+++ BE/src/main/java/team25/issuetracker/OauthRestController.java
@@ -0,0 +1,30 @@
+package team25.issuetracker;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OauthRestController {
+
+	private final OauthService oauthService;
+
+	public OauthRestController(OauthService oauthService) {
+		this.oauthService = oauthService;
+	}
+
+	/**
+ 	 * @param provider API 요청시 해당되는 파티 입력
+	 * @param code 프론트에서 지니고 있는 Authorization Code 받기
+	 */
+	@GetMapping("/login/oauth/{provider}")
+	public ResponseEntity<LoginResponse> login(@PathVariable String provider,
+		@RequestParam String code) {
+		LoginResponse loginResponse = oauthService.login(provider, code);
+
+		System.out.println(loginResponse.getEmail());
+		return ResponseEntity.ok().body(loginResponse);
+	}
+}
