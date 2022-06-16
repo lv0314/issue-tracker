3b394a7e6eac8ef5cf76faa8ac533d4d2e0c753f
tokaji <fpzk5656@gmail.com>
2022-06-16 14:32:14 +0900

    refactor: sout 출력 부분 log 로 변경

--- BE/src/main/java/team25/issuetracker/OauthRestController.java
+++ BE/src/main/java/team25/issuetracker/OauthRestController.java
@@ -1,11 +1,13 @@
 package team25.issuetracker;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class OauthRestController {
 
@@ -16,15 +18,15 @@ public class OauthRestController {
 	}
 
 	/**
- 	 * @param provider API 요청시 해당되는 파티 입력
-	 * @param code 프론트에서 지니고 있는 Authorization Code 받기
+	 * @param provider API 요청시 해당되는 파티 입력
+	 * @param code     프론트에서 지니고 있는 Authorization Code 받기
 	 */
 	@GetMapping("/login/oauth/{provider}")
 	public ResponseEntity<LoginResponse> login(@PathVariable String provider,
 		@RequestParam String code) {
 		LoginResponse loginResponse = oauthService.login(provider, code);
 
-		System.out.println(loginResponse.getEmail());
+		log.info("login email: " + loginResponse.getEmail());
 		return ResponseEntity.ok().body(loginResponse);
 	}
 }
