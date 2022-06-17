d69b3ef44819fa726092258772f9ed6918516d4c
godhyeongman <aaaa4000@naver.com>
2022-06-17 20:35:30 +0900

    feat: 로그인 관련 컴포넌트 구현

--- FE/src/pages/Login/index.tsx
+++ FE/src/pages/Login/index.tsx
@@ -1,21 +1,28 @@
 import * as S from './style';
 import { Button } from '@/components/common/Button';
 
+const GITHUB_CLIENT_ID = 'e491cb1179a89880960b';
+const GITHUB_LOGIN_URL = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=read:user user:email`;
+
 export function Login() {
+  function clickHandler() {
+    window.location.href = GITHUB_LOGIN_URL;
+  }
+
   return (
     <S.LoginLayOut>
       <Button
-        text="Github 계정으로 로그인"
         size="large"
         color="black"
         fontSize="large"
-      />
-      <Button
-        text="아이디로 로그인"
-        size="large"
-        color="blue"
-        fontSize="large"
-      />
+        // eslint-disable-next-line react/jsx-no-bind
+        onClick={clickHandler}
+      >
+        Github 계정으로 로그인
+      </Button>
+      <Button size="large" color="blue" fontSize="large">
+        아이디 로그인
+      </Button>
     </S.LoginLayOut>
   );
 }
