f19d8174945b5c8ef8773aa2982301ed0177bcf0
ver <luzverde0314@gmail.com>
2022-06-22 18:14:14 +0900

    feat: 유저 관련 디렉토리 생성 및 컴포넌트 구현

---
+++ FE/src/components/User/AuthorFilterDetail.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+import { Icon } from '../common/Icon';
+import { Text } from '../common/Text';
+import UNDER_ARROW from '@/assets/UnderArrow.svg';
+import { ListModal } from '../common/ListModal';
+import { AssigneeFilterItem } from './AssigneeFilterItem';
+
+type AuthorFilterDetailProps = {
+  userData: { name: string; imgUrl?: string }[];
+};
+
+const SortDetail = styled.details`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  summary {
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    gap: 4px;
+    min-width: 80px;
+  }
+`;
+
+export function AuthorFilterDetail({ userData }: AuthorFilterDetailProps) {
+  return (
+    <SortDetail>
+      <summary>
+        <Text text="작성자" fontWeight="bold" color="label" />
+        <Icon iconName={UNDER_ARROW} iconSize="base" />
+      </summary>
+      <ListModal listTitle="작성자 필터">
+        {userData.map(({ name, imgUrl }) => (
+          <AssigneeFilterItem name={name} key={`${name}${Date.now()}`} />
+        ))}
+      </ListModal>
+    </SortDetail>
+  );
+}
