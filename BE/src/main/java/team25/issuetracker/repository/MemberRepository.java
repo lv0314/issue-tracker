f499c7290fed93ed62acab22a9cdcf4bfcf91562
tokaji <fpzk5656@gmail.com>
2022-06-24 13:37:59 +0900

    feat: RefreshTokenRepository 추가

--- BE/src/main/java/team25/issuetracker/repository/MemberRepository.java
+++ BE/src/main/java/team25/issuetracker/repository/MemberRepository.java
@@ -2,8 +2,10 @@ package team25.issuetracker.repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import team25.issuetracker.domain.Member;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
 	Optional<Member> findByOauthId(String id);
