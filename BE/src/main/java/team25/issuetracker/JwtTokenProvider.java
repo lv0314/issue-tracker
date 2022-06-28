87a9985a13972c84dc8b3c271266cf9e68e08be2
tokaji <fpzk5656@gmail.com>
2022-06-28 23:46:22 +0900

    refactor: JwtTokenProvider에서 role 사용하던 부분 삭제 및 try catch문은 optional로 대체

--- BE/src/main/java/team25/issuetracker/JwtTokenProvider.java
+++ BE/src/main/java/team25/issuetracker/JwtTokenProvider.java
@@ -8,6 +8,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.Optional;
 import java.util.Random;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -15,22 +16,23 @@ import team25.issuetracker.domain.OauthRefreshToken;
 
 @Component
 public class JwtTokenProvider {
+
 	@Value("${jwt.access-token.expire-length:10000}")
-	private long accessTokenValidityInMilliseconds;
+	private long ACCESS_TOKEN_VALIDITY_IN_MILLISECONDS;
 	@Value("${jwt.refresh-token.expire-length:10000}")
-	private long refreshTokenValidityInMilliseconds;
+	private long REFRESH_TOKEN_VALIDITY_IN_MILLISECONDS;
 	@Value("${jwt.token.secret-key:secret-key}")
 	private String secretKey;
 
 	public String createAccessToken(String payload) {
-		return createToken(payload, accessTokenValidityInMilliseconds);
+		return createToken(payload, ACCESS_TOKEN_VALIDITY_IN_MILLISECONDS);
 	}
 
 	public String createRefreshToken() {
 		byte[] array = new byte[7];
 		new Random().nextBytes(array);
 		String generatedString = new String(array, StandardCharsets.UTF_8);
-		return createToken(generatedString, refreshTokenValidityInMilliseconds);
+		return createToken(generatedString, REFRESH_TOKEN_VALIDITY_IN_MILLISECONDS);
 	}
 
 	public String createToken(String payload, long expireLength) {
@@ -48,7 +50,8 @@ public class JwtTokenProvider {
 
 	public String getPayload(String token) {
 		try {
-			return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+			return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody()
+				.getSubject();
 		} catch (ExpiredJwtException e) {
 			return e.getClaims().getSubject();
 		} catch (JwtException e) {
@@ -66,39 +69,28 @@ public class JwtTokenProvider {
 		}
 	}
 
-	public String validateRefreshToken(OauthRefreshToken refreshTokenObj){
+	public String validateRefreshToken(OauthRefreshToken refreshTokenObj) {
 
 		String refreshToken = refreshTokenObj.getRefreshToken();
 
-		try {
-			// 검증
-			Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(refreshToken);
+		// 검증
+		Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(refreshToken);
 
-			//refresh 토큰의 만료시간이 지나지 않았을 경우, 새로운 access 토큰을 생성합니다.
-			if (!claims.getBody().getExpiration().before(new Date())) {
-				return recreationAccessToken(claims.getBody().get("sub").toString(), claims.getBody().get("roles"));
-			}
-		}catch (Exception e) {
-
-			//refresh 토큰이 만료되었을 경우, 로그인이 필요합니다.
-			return null;
-
-		}
-
-		return null;
+		//refresh 토큰의 만료시간이 지나지 않았을 경우, 새로운 access 토큰을 생성합니다.
+		return Optional.of(recreationAccessToken(claims.getBody().get("sub").toString()))
+			.filter(v -> !claims.getBody().getExpiration().before(new Date())).orElse(null);
 	}
 
-	public String recreationAccessToken(String userEmail, Object roles){
-
+	public String recreationAccessToken(String userEmail) {
 		Claims claims = Jwts.claims().setSubject(userEmail); // JWT payload 에 저장되는 정보단위
-		claims.put("roles", roles); // 정보는 key / value 쌍으로 저장된다.
 		Date now = new Date();
 
 		//Access Token
 		String accessToken = Jwts.builder()
 			.setClaims(claims) // 정보 저장
 			.setIssuedAt(now) // 토큰 발행 시간 정보
-			.setExpiration(new Date(now.getTime() + accessTokenValidityInMilliseconds)) // set Expire Time
+			.setExpiration(
+				new Date(now.getTime() + ACCESS_TOKEN_VALIDITY_IN_MILLISECONDS)) // set Expire Time
 			.signWith(SignatureAlgorithm.HS256, secretKey)  // 사용할 암호화 알고리즘과
 			// signature 에 들어갈 secret값 세팅
 			.compact();
