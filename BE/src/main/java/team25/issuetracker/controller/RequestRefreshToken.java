073be30f77521c4f52710a6daea9134abb3bce64
tokaji <fpzk5656@gmail.com>
2022-06-28 23:42:46 +0900

    feat: 리프레쉬 요청 DTO

---
+++ BE/src/main/java/team25/issuetracker/controller/RequestRefreshToken.java
@@ -0,0 +1,11 @@
+package team25.issuetracker.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RequestRefreshToken {
+
+	private String refreshToken;
+}
