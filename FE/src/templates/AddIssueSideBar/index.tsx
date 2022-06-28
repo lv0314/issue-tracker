c174e5539c723f83e8bf5c9f43205bbda7a6cbc5
godhyeongman <aaaa4000@naver.com>
2022-06-28 11:10:03 +0900

    refactor: 사이드 바 컴포넌트 리코일 기반 상태관리 적용 및 컴포넌트 분리

--- FE/src/templates/AddIssueSideBar/index.tsx
+++ FE/src/templates/AddIssueSideBar/index.tsx
@@ -1,21 +1,31 @@
 import { useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import * as S from './style';
-import { assigneeList } from '@/recoil/atoms/addIssueAssigneeList';
+import { assigneeList } from '@/recoil/atoms/assigneeData';
+import { labelData as LabelData } from '@/recoil/atoms/LabelData';
 import { Text } from '@/components/common/Text';
+import { ListModal } from '@/components/common/ListModal';
 import { AssigneeListItem } from '@/components/User/AssigneeListItem';
+import { LabelListItem } from '@/components/Label/LabelListItem';
 
 export function AddIssueSideBar() {
-  const issueAssigneeList = useRecoilValue(assigneeList);
+  const assigneeData = useRecoilValue(assigneeList);
+  const labelData = useRecoilValue(LabelData);
   // const issueLabelist = useRecoilValue();
   // const issueMilestoneList = useRecoilValue();
   // const [assigneeDetailSummary, setAssigneeDeatilSummary] = useState(null);
   // const [labelDetailSummary, setLabelDetailSummary] = useState(null);
   // const [MilestoneDetailSummary, setMilestoneDeatilSummary] = useState(null);
 
-  const sideBarAssigneeList = issueAssigneeList
-    ? issueAssigneeList.map(({ name, profileImage }) => (
+  const sideBarAssigneeList = assigneeData
+    ? assigneeData.map(({ name, profileImage }) => (
         <AssigneeListItem assignee={name} assigneeProfileImg={profileImage} />
+      ))
+    : null;
+
+  const sideBarLabelList = labelData
+    ? labelData.map(({ color, name }) => (
+        <LabelListItem color={color} name={name} />
       ))
     : null;
 
@@ -26,20 +36,20 @@ export function AddIssueSideBar() {
           <Text text="담당자" />
           <Text text="아이콘" />
         </summary>
-        <S.OptionMenu>
-          <S.OptionHeader>담당자 추가</S.OptionHeader>
-          <S.OptionList>{sideBarAssigneeList}</S.OptionList>
-        </S.OptionMenu>
+        <ListModal listTitle="담당자 추가">{sideBarAssigneeList}</ListModal>
       </S.OptionDetail>
+
       <S.OptionDetail>
         <summary>
-          <Text text="담당자" />
+          <Text text="레이블" />
           <Text text="아이콘" />
         </summary>
+        <ListModal listTitle="라벨 추가">{sideBarLabelList}</ListModal>
       </S.OptionDetail>
+
       <S.OptionDetail>
         <summary>
-          <Text text="담당자" />
+          <Text text="마일스톤" />
           <Text text="아이콘" />
         </summary>
       </S.OptionDetail>
