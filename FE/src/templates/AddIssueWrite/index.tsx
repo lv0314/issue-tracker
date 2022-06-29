2cf64bacd5188f208c4fa0c6fc804ea3a82fc058
godhyeongman <aaaa4000@naver.com>
2022-06-29 10:57:00 +0900

    feat: deferedValue 적용한 키보드 타입 상태 변경 지연 구현

--- FE/src/templates/AddIssueWrite/index.tsx
+++ FE/src/templates/AddIssueWrite/index.tsx
@@ -1,33 +1,49 @@
+import { useRecoilState } from 'recoil';
+import { useDeferredValue, useMemo } from 'react';
 import { Badge } from '@/components/common/Badge';
 import * as S from './style';
 import { Text } from '@/components/common/Text';
 import PAPERCLIP_ICON from '@/assets/Icons/paperclip.svg';
+import { addIssue } from '@/recoil/atoms/addIssue';
 
 export function AddIssueWrite() {
-  return (
-    <S.AddIssueWrite>
-      <Badge
-        imgSRC="https://ca.slack-edge.com/T74H5245A-U02RA7K501L-3c55992367ca-512"
-        alt="profile image"
-      />
-      <S.IssueType>
-        <S.IssueTypeTitle placeholder="제목" />
-        <S.IssueTypeContents>
-          <S.IssueTextArea placeholder="코멘트를 입력하세요" />
-          <S.AttachFile>
-            <S.AttachFileLabel>
-              <PAPERCLIP_ICON />
-              <input type="file" />
-            </S.AttachFileLabel>
-            <Text
-              text="파일 첨부하기"
-              fontSize="smaller"
-              color="label"
-              fontWeight="bold"
-            />
-          </S.AttachFile>
-        </S.IssueTypeContents>
-      </S.IssueType>
-    </S.AddIssueWrite>
+  const [issueWrite, setIssueWrite] = useRecoilState(addIssue);
+  const deferredIssueData = useDeferredValue(issueWrite);
+
+  const handleIssueWrite = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newTitle = e.target.value;
+
+    setIssueWrite({ ...deferredIssueData, issueTitle: newTitle });
+  };
+  const deferedIssueWrite = useMemo(
+    () => (
+      <S.AddIssueWrite>
+        <Badge
+          imgSRC="https://ca.slack-edge.com/T74H5245A-U02RA7K501L-3c55992367ca-512"
+          alt="profile image"
+        />
+        <S.IssueType>
+          <S.IssueTypeTitle placeholder="제목" onChange={handleIssueWrite} />
+          <S.IssueTypeContents>
+            <S.IssueTextArea placeholder="코멘트를 입력하세요" />
+            <S.AttachFile>
+              <S.AttachFileLabel>
+                <PAPERCLIP_ICON />
+                <input type="file" />
+              </S.AttachFileLabel>
+              <Text
+                text="파일 첨부하기"
+                fontSize="smaller"
+                color="label"
+                fontWeight="bold"
+              />
+            </S.AttachFile>
+          </S.IssueTypeContents>
+        </S.IssueType>
+      </S.AddIssueWrite>
+    ),
+    [deferredIssueData],
   );
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  return <>{deferedIssueWrite}</>;
 }
