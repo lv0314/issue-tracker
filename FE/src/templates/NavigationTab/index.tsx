ddebf7d0f7189d736d70c772b5c742d31a014c89
ver <luzverde0314@gmail.com>
2022-07-01 11:43:41 +0900

    refactor: NavigationTab 분리

--- FE/src/templates/NavigationTab/index.tsx
+++ FE/src/templates/NavigationTab/index.tsx
@@ -1,9 +1,7 @@
 import { useRecoilValue } from 'recoil';
-import { Link } from 'react-router-dom';
 import * as S from './style';
 import { LabelNavItem } from '@/components/Label/LabelNavItem';
 import { MilestonNavItem } from '@/components/Milestone/MilestoneNavItem';
-import { Button } from '@/components/common/Button';
 import { getLabels } from '@/recoil/selectors/getLabels';
 import { getMilestones } from '@/recoil/selectors/getMilestones';
 
@@ -12,16 +10,9 @@ export function NavigationTab() {
   const { milestones } = useRecoilValue(getMilestones);
 
   return (
-    <S.Layout>
-      <S.NavigationTab>
-        <LabelNavItem labelLength={labels.length} />
-        <MilestonNavItem milestoneLength={milestones.length} />
-      </S.NavigationTab>
-      <Link to="/issueAdd">
-        <Button size="small" color="primary" fontSize="base">
-          이슈 작성
-        </Button>
-      </Link>
-    </S.Layout>
+    <S.NavigationTab>
+      <LabelNavItem labelLength={labels.length} />
+      <MilestonNavItem milestoneLength={milestones.length} />
+    </S.NavigationTab>
   );
 }
